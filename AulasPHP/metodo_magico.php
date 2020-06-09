9b5dd9e4f258c9e2976a9ccedea4f698c0363d88
Juliana Reis <julianareis.cont@gmail.com>
2020-06-08 22:33:15 -0300

    Finalização da aula sobre métodos mágicos

--- AulasPHP/metodo_magico.php
+++ AulasPHP/metodo_magico.php
@@ -5,15 +5,29 @@ class Endereco {
     private $cidade;
     private $numero;
 
+    //Metodo para construir um objeto
     public function __construct($a, $b, $c){
         $this->lougradouro = $a;
         $this->cidade = $b;
         $this->numero = $c;
     }
+
+    //Metodo para destruir um objeto
+    //Sempre será a última função a ser chamada
+    public function __destruct(){
+        var_dump("destruir");
+    }
+    
+    //Converter para String - Função para exibir os dados do objeto
+    public function __toString(){
+        return $this->lougradouro.", ".$this->cidade.", ".$this->numero;
+    }
 }
 
 $meuendereco = new Endereco('Beco 1', 'Porto Alegre', 10);
 var_dump($meuendereco);
+        
+// Função para matar a variável 
+//unset($meuendereco);
 
-
-?>
+echo $meuendereco;
