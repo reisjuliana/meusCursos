31aaf46c75ad9726add810a54c4183cf4ed081c1
Juliana Reis <julianareis.cont@gmail.com>
2020-06-18 22:30:56 -0300

    aulas de php

---
+++ AulasPHP/DAO/Sql.php
@@ -0,0 +1,38 @@
+<?php
+
+class Sql extends PDO {
+
+    private $conn;
+    // Conexão de banco:
+    public function __construct(){
+        $this->conn = new PDO("mysql:dbname=meuBanco;host=localhost", "root", "");
+    }
+
+    private function setParams($statment, $parameters = array()){
+        foreach($parameters as $key => $value){
+            $this->setParam($key, $value);
+        }
+    }
+    private function setParam($statment, $key, $value){
+        $statment->bindParam($key, $value);
+    }
+
+    public function query($rawQuery, $params = array()){
+        $stmt = $this->conn->prepare($rawQuery);
+        $this->setParams($stmt,$params);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function select($rawQuery, $params = array()):array{
+        $stmt = $this->query($rawQuery, $params);
+        return  $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+}
+
+
+
+
+
+?>
